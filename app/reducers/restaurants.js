86eabbc1aad1d1442f7ff80b49c27ddbfe0e6ae8
toran billups <toranb@gmail.com>
2018-03-02 17:44:41 -0600

    revert back to JavaScript

---
+++ app/reducers/restaurants.js
@@ -0,0 +1,40 @@
+import _ from 'lodash';
+import reselect from 'reselect';
+
+const { createSelector } = reselect;
+
+const initialState = {
+  all: undefined,
+  selectedId: undefined
+};
+
+export default ((state, action) => {
+  switch(action.type) {
+    case 'RESTAURANTS:TRANSFORM_DETAIL': {
+      const number = action.number;
+      return Object.assign({}, state, {selectedId: number + 1});
+    }
+    case 'RESTAURANTS:TRANSFORM_LIST': {
+      const restaurants = _.keyBy(action.response, (restaurant) => restaurant.id);
+      const merged = _.extend({}, state.all, restaurants);
+      return Object.assign({}, state, {all: merged});
+    }
+    default: {
+      return state || initialState;
+    }
+  }
+});
+
+const all = (state) => state.restaurants.all;
+const selectedId = (state) => state.restaurants.selectedId;
+
+export const getRestaurants = createSelector(
+  all,
+  (all) => all
+);
+
+export const getSelectedRestaurant = createSelector(
+  all,
+  selectedId,
+  (all, selectedId) => _.get(all, selectedId)
+);
