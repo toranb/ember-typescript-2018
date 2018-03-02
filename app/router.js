9432a1a793144c5f2d19798a19d197d4a992ca32
toran billups <toranb@gmail.com>
2018-03-02 17:38:43 -0600

    working app w/ typescript

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('restaurants', {path: '/'}, function() {
+    this.route('detail', {path: '/detail/:id'});
+  });
 });
 
 export default Router;
